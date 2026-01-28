9677556157c2648404d633732724bd8bc14ce7cd
Bernydotjar <fearlesed@gmail.com>
2026-01-27 22:01:27 -0600

    feat: add chess camp vol1 ordered exercises dataset

--- src/utils/exercises/index.ts
+++ src/utils/exercises/index.ts
@@ -1,3 +1,5 @@
 import { BLOCK1_ROOK } from './block1';
+import chessCampVol1 from '../../data/chess_camp_vol1_624_exercises_ordered.json';
 
 export const EXERCISES = BLOCK1_ROOK;
+export const CHESS_CAMP_VOL1_ORDERED = chessCampVol1;
